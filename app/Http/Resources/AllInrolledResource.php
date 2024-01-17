8d77bbbe381c5d296d2e30a04571496f63c35f85
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2024-01-17 07:25:39 -0500

    fix redsource

--- app/Http/Resources/AllInrolledResource.php
+++ app/Http/Resources/AllInrolledResource.php
@@ -15,6 +15,7 @@ class AllInrolledResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->user->full_name,
             'phone' => $this->user->phone,
             'course_title' => $this->course->name,
