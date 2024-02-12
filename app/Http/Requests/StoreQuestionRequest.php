2abdb48c69f31dcdbda5e8bae538b914de8f19ec
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2024-02-12 16:54:19 -0500

    create question request

--- app/Http/Requests/StoreQuestionRequest.php
+++ app/Http/Requests/StoreQuestionRequest.php
@@ -26,7 +26,7 @@ class StoreQuestionRequest extends FormRequest
             'image' => 'required_without:title|image|mimes:jpg,png,jpeg',
             'clarification_text' => 'required_without:clarification_image|string',
             'clarification_image' => 'required_without:clarification_text|image|mimes:jpg,png,jpeg',
-            'choices' => 'required|array',
+//            'choices' => 'required|array',
 //            'choices.*.title' => 'required_without:choices.*.image|string',
 //            'choices.*.image' => 'required_without:choices.*.title|image|mimes:jpg,png,jpeg',
 //            'choices.*.is_true' => 'required|boolean',
