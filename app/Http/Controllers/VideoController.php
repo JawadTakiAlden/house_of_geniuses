9848456cbf5e164c67c77f59c372e9001cc71bcf
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2024-02-16 07:08:52 -0500

    create simple quiz resource

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -12,15 +12,19 @@ class VideoController extends Controller
     use HTTPResponse;
 
     public function getVideos () {
-        $client = new Vimeo("f5558ea3eb98817fbe2126ae2541b6e11bfb0e44"
-            , "bXD6qM5hnj79bWmodVz7vIw4ZS1maOBgVUl8N5cWnV2r6aTMZ6QNI3UT5LwW+lOSLRH2vvfoE1xoAuKfsjNbe+pEjXwaSXt+7XCPxaJzNWQJi/GWUbkd3o4DcM307fP9",
-            "0090c4cf290951714e846847fe8b2fe5");
+        try {
+            $client = new Vimeo("f5558ea3eb98817fbe2126ae2541b6e11bfb0e44"
+                , "bXD6qM5hnj79bWmodVz7vIw4ZS1maOBgVUl8N5cWnV2r6aTMZ6QNI3UT5LwW+lOSLRH2vvfoE1xoAuKfsjNbe+pEjXwaSXt+7XCPxaJzNWQJi/GWUbkd3o4DcM307fP9",
+                "0090c4cf290951714e846847fe8b2fe5");
 
-        $response = $client->request('/users/111781459/videos', array(), 'GET');
-        $responseData = $response['body'];
-        $videos = $responseData['data'];
-        $test = collect($videos);
-        return $this->success(VideoResource::collection($test));
+            $response = $client->request('/users/111781459/videos', array(), 'GET');
+            $responseData = $response['body'];
+            $videos = $responseData['data'];
+            $test = collect($videos);
+            return $this->success(VideoResource::collection($test));
+        }catch (\Throwable $th){
+            return $this->error($th->getMessage() , 500);
+        }
     }
 
     public function watch($videoID){
