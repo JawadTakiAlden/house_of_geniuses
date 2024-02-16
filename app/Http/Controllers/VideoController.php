8d3a4348f2d4afe37ac54b7f36b8133e7e00cc0b
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2024-02-16 06:48:19 -0500

    create simple quiz resource

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -20,7 +20,7 @@ class VideoController extends Controller
         $responseData = $response['body'];
         $videos = $responseData['data'];
         $test = collect($videos)->map(fn($vid) =>
-            dd($vid)
+            $vid['name']
         );
         return $test;
     }
