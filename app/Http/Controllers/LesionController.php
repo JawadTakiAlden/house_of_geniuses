0053a881ff39c1ee608aa1677595be7184c53e02
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2024-02-16 09:01:54 -0500

    create simple quiz resource

--- app/Http/Controllers/LesionController.php
+++ app/Http/Controllers/LesionController.php
@@ -81,7 +81,8 @@ class LesionController extends Controller
         try {
             $type = $request->type;
             if ($type === 'pdf'){
-
+                $file = $request->file('pdfFile');
+                return $file;
             }else if ($type === 'video'){
                 $client = new Vimeo("f5558ea3eb98817fbe2126ae2541b6e11bfb0e44"
                     , "bXD6qM5hnj79bWmodVz7vIw4ZS1maOBgVUl8N5cWnV2r6aTMZ6QNI3UT5LwW+lOSLRH2vvfoE1xoAuKfsjNbe+pEjXwaSXt+7XCPxaJzNWQJi/GWUbkd3o4DcM307fP9",
@@ -89,13 +90,19 @@ class LesionController extends Controller
 
                 $response = $client->request($request->videoURI, array(), 'GET');
                 $responseData = $response['body'];
-                return $responseData['uri'];
+                $lesion = Lesion::create($request->only([
+                    'title' => $responseData['name'],
+                    'link' => $responseData['player_embed_url'],
+                    'time' => intval($responseData['duration']),
+                    'is_open' => $request->is_open,
+                    'is_visible' => $request->is_visible,
+                    'type' => $type,
+                    'chapter_id' => $request->chapter_id
+                ]));
+                return $this->success(LesionResource::make($lesion) , $lesion->title . ' added successfully to ' . $lesion->chapter->name . ' in ' . $lesion->chapter->course->name .' course');
             }else{
-
+                return $this->error('you are provide unknown type of lesion' , 422);
             }
-
-//            $lesion = Lesion::create($request->only(['title' , 'link' , 'type' , 'is_open' , 'is_visible' , 'chapter_id' , 'time']));
-//            return $this->success(LesionResource::make($lesion) , $lesion->title . ' added successfully to ' . $lesion->chapter->name . ' in ' . $lesion->chapter->course->name .' course');
         }catch (\Throwable $th){
             return $this->catchError($th);
         }
