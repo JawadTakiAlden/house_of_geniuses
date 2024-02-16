a1a16ca5da0bb90665438c2c95cf620de9e7e9e4
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2024-02-16 06:20:09 -0500

    create simple quiz resource

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -19,7 +19,7 @@ class VideoController extends Controller
         $response = $client->request('/me/videos', array(), 'GET');
         $responseData = $response['body'];
         $videos = $responseData['data'];
-        return $this->success(collect($videos));
+        return $this->success(collect($videos)->data);
     }
 
     public function watch($videoID){
