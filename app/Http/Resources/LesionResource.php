295738aed019c4f609a4d7bce7141a4b543070ad
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2024-02-16 10:59:12 -0500

    create simple quiz resource

--- app/Http/Resources/LesionResource.php
+++ app/Http/Resources/LesionResource.php
@@ -18,7 +18,7 @@ class LesionResource extends JsonResource
         return [
             'id' => intval($this->id),
             'title' => $this->title,
-            'link' => $this->link,
+            'link' => $this->type === 'pdf' ? asset('storage/'.$this->link) : $this->link,
             'time' => intval($this->time),
             'is_open' => boolval($this->is_open),
             'is_visible' => boolval($this->is_visible),
