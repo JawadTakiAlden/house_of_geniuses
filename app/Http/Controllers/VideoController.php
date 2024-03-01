f58b24b96f2389f78235a128e0f938f88b5de53a
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2024-03-01 15:22:47 -0500

    videi link watch

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -13,6 +13,7 @@ class VideoController extends Controller
 
     public function getVideos () {
         try {
+//            GEThttps://api.vimeo.com/videos/{video_id}?fields=play
             $client = new Vimeo("1a223f522e28ec1fb6b9e8e25f088348ecf1a6ef"
                 , "xEjIb7Q0J2zYJRfsgpb1XRcwG2XRig/Nm5Gr3nejJMuFuuLGxr1lx0Z2A7kHN8MOvMPHhLG+pOX5fI5bk7WC5YIvQsPpv+9/pM2a8UlyqQOCfg7VqtGRZ9qtlHcOUH3t",
                 "b666b813b6109e0574302a8d4237445a");
@@ -31,8 +32,8 @@ class VideoController extends Controller
         $client = new Vimeo("f5558ea3eb98817fbe2126ae2541b6e11bfb0e44"
             , "bXD6qM5hnj79bWmodVz7vIw4ZS1maOBgVUl8N5cWnV2r6aTMZ6QNI3UT5LwW+lOSLRH2vvfoE1xoAuKfsjNbe+pEjXwaSXt+7XCPxaJzNWQJi/GWUbkd3o4DcM307fP9",
             "0090c4cf290951714e846847fe8b2fe5");
-        $video = $client->request($videoID);
+        $video = $client->request($videoID.'?field=play');
         $data = $video['body'];
-        return view('video.watch', compact('data'));
+        return $data;
     }
 }
