5bdef421c6fffc30a085244b4479ec51fcbc5224
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2024-03-01 15:59:02 -0500

    videi link watch

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -45,16 +45,7 @@ class VideoController extends Controller
 
     public function download($videoID){
         $video = $this->client->request($videoID.'?fields=download');
-        $data = collect($video['body'])->first();
-//        $data = collect($video['download'])->map(fn($download) =>
-//            [
-//                'link' => $download['link'],
-//                'type' => $download['type'],
-//                'rendition' => $download['rendition'],
-//                'size_short' => $download['size_short'],
-//
-//            ]
-//        );
+        $data = $video['body'];
         return response([
             'link' => $data
         ] , 200);
