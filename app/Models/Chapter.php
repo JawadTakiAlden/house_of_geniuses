4c0aa19648f1dad6bad42298807940de95f4ec7e
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2024-03-02 15:45:55 -0500

    check code api

--- app/Models/Chapter.php
+++ app/Models/Chapter.php
@@ -23,6 +23,6 @@ class Chapter extends Model
     }
 
     public function quizzes(){
-        return $this->belongsToMany(Quiz::class)->withPivot(['id' , 'is_visible']);
+        return $this->belongsToMany(Quiz::class)->withPivot(['id' , 'is_visible' , 'is_free']);
     }
 }
