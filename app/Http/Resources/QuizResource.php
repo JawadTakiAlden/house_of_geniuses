05797317cc6aa7d74d23cb52f06fb0604868d553
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2024-03-02 16:00:51 -0500

    check code api

--- app/Http/Resources/QuizResource.php
+++ app/Http/Resources/QuizResource.php
@@ -16,10 +16,17 @@ class QuizResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [
+        $pivot = $this->pivot;
+        $base = [
             'title' => $this->title,
             'description' => $this->description,
             'questions' => $request->user()->type === UserType::ADMIN ? QuestionResource::collection($this->forAdminQuestions) : QuestionResource::collection($this->forUserQuestions)
         ];
+        if ($pivot){
+            $base = array_merge($base , [
+                'is_free' => $pivot->is_free
+            ]);
+        }
+        return $base;
     }
 }
