a2840a5753b9bc08af4a3aa835da54e1b1ce701b
JawadTakiAlden <jawad.taki.alden2002@gmail.com>
2024-07-07 17:08:48 +0300

    firebase

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -7,6 +7,11 @@ use App\Http\Requests\SendNotificationRequest;
 use App\HttpResponse\HTTPResponse;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Kreait\Firebase\Exception\FirebaseException;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\Messaging\CloudMessage;
+use Kreait\Firebase\Messaging\Notification;
+use Illuminate\Support\Facades\Log;
 
 class NotificationController extends Controller
 {
@@ -14,43 +19,68 @@ class NotificationController extends Controller
 
     use HTTPResponse;
     public function BasicSendNotification($title , $body , $FcmToken){
-        $url = 'https://fcm.googleapis.com/fcm/send';
-        $server_key = config('app.firebase_server_key');
-        $date = [
-            'registration_ids' => $FcmToken,
+
+        $firebase = (new Factory())
+            ->withServiceAccount(config_path('firebase_config.json'));
+
+        $this->messaging = $firebase->createMessaging();
+
+
+        $messageData = [
             'notification' => [
                 'title' => $title,
                 'body' => $body
-            ]
+            ],
         ];
 
-        $encodedData = json_encode($date);
+        $message = CloudMessage::fromArray($messageData);
 
-        $headers = [
-            'Authorization: key='.$server_key,
-            'Content-Type: application/json'
-        ];
-
-        $ch = curl_init();
-
-        curl_setopt($ch , CURLOPT_URL , $url);
-        curl_setopt($ch , CURLOPT_POST , true);
-        curl_setopt($ch , CURLOPT_HTTPHEADER , $headers);
-        curl_setopt($ch , CURLOPT_RETURNTRANSFER , true);
-        curl_setopt($ch , CURLOPT_SSL_VERIFYHOST , 0);
-        curl_setopt($ch , CURLOPT_HTTP_VERSION , CURL_HTTP_VERSION_1_1);
-        curl_setopt($ch , CURLOPT_SSL_VERIFYPEER , false);
-        curl_setopt($ch , CURLOPT_POSTFIELDS , $encodedData);
-
-        $result = curl_exec($ch);
-
-        if ($result === FALSE){
-            return HelperFunction::ServerErrorResponse();
+        try {
+            $this->messaging->sendMulticast($message, $FcmToken);
+            return $this->success(null , __('messages.notification_controller.send_successfully'));
+        } catch (\Kreait\Firebase\Exception\MessagingException $e) {
+            Log::error($e->getMessage());
+        } catch (FirebaseException $e) {
+            Log::error($e->getMessage());
         }
 
-        curl_close($ch);
-
-        return $this->success($result , __('messages.notification_controller.send_successfully'));
+//        $url = 'https://fcm.googleapis.com/fcm/send';
+//        $server_key = config('app.firebase_server_key');
+//        $date = [
+//            'registration_ids' => $FcmToken,
+//            'notification' => [
+//                'title' => $title,
+//                'body' => $body
+//            ]
+//        ];
+//
+//        $encodedData = json_encode($date);
+//
+//        $headers = [
+//            'Authorization: key='.$server_key,
+//            'Content-Type: application/json'
+//        ];
+//
+//        $ch = curl_init();
+//
+//        curl_setopt($ch , CURLOPT_URL , $url);
+//        curl_setopt($ch , CURLOPT_POST , true);
+//        curl_setopt($ch , CURLOPT_HTTPHEADER , $headers);
+//        curl_setopt($ch , CURLOPT_RETURNTRANSFER , true);
+//        curl_setopt($ch , CURLOPT_SSL_VERIFYHOST , 0);
+//        curl_setopt($ch , CURLOPT_HTTP_VERSION , CURL_HTTP_VERSION_1_1);
+//        curl_setopt($ch , CURLOPT_SSL_VERIFYPEER , false);
+//        curl_setopt($ch , CURLOPT_POSTFIELDS , $encodedData);
+//
+//        $result = curl_exec($ch);
+//
+//        if ($result === FALSE){
+//            return HelperFunction::ServerErrorResponse();
+//        }
+//
+//        curl_close($ch);
+//
+//
     }
 
     public function sendNotificationForAllUser(SendNotificationRequest $request){
