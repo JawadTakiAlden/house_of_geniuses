45b240a66a8d0793d341831f08cb5c0ef11929d7
JawadTakiAlden <jawad.taki.alden2002@gmail.com>
2024-07-10 16:49:01 +0300

    get veidos from two account

--- app/Http/Controllers/LesionController.php
+++ app/Http/Controllers/LesionController.php
@@ -104,10 +104,9 @@ class LesionController extends Controller
                 return $this->success(  LesionResource::make($lesion),$lesion->title . __("messages.lesion_controller.create"));
             }
             else if ($type === 'video'){
-                return $request->videoURI;
                 $response = $this->client2->request($request->videoURI, array(), 'GET');
-                $responseData = $response['body'];
-                return $responseData;
+//                $responseData = $response['body'];
+                return $response;
                 $date = [
                     'description' => $responseData['description'],
                     'link' => $responseData['uri'],
