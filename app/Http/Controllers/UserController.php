f690e20636982dbc88712576b60a53a5ab0d5242
JawadTakiAlden <jawad.taki.alden2002@gmail.com>
2024-09-21 17:09:23 +0300

    fix watch video api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -239,9 +239,9 @@ class UserController extends Controller
             if (!$user){
                 return HelperFunction::notFoundResponce();
             }
-            if (Gate::denies('get_courses_of_user' , $user)){
-                return $this->permissionDenide();
-            }
+//            if (Gate::denies('get_courses_of_user' , $user)){
+//                return $this->permissionDenide();
+//            }
             return $this->success(CourseResource::collection($user->inroledCorurses));
         }catch(\Throwable $th){
 //            return HelperFunction::ServerErrorResponse();
