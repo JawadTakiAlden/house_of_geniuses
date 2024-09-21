07711dcb77fb12758cead0c32fa488a5bbded15a
JawadTakiAlden <jawad.taki.alden2002@gmail.com>
2024-09-21 16:44:06 +0300

    make watch video authorize

--- app/Http/Requests/WatchVideoRequest.php
+++ app/Http/Requests/WatchVideoRequest.php
@@ -11,7 +11,7 @@ class WatchVideoRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,7 @@ class WatchVideoRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'source' => "in:vimeo-1,vimeo-2"
         ];
     }
 }
