6e17fa58f6a71aeeb2f3c373e557ecf10232d46b
JawadTakiAlden <jawad.taki.alden2002@gmail.com>
2024-10-08 11:37:46 +0300

    fix

--- resources/views/download_app/download.blade.php
+++ resources/views/download_app/download.blade.php
@@ -6,7 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" href="./logo2.jpg">
-    <link rel="apple-touch-icon" href="./logo2.png" />
+    <link rel="apple-touch-icon" href="./logo2.jpg" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <meta
         name="description"
         content="منصة بيت العباقرة للتعليم الالكتروني
@@ -24,52 +25,47 @@
         href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap"
         rel="stylesheet"
     />
-    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="./app.css">
     <title>House of geniuses</title>
 </head>
 <body>
     <main>
         <div class="main-content">
-            <div class="w-[50%] flex items-center justify-center">
-                <img
-                    src="./logo2.jpg"
-                    class="w-full md:max-w-[400px] rounded-[0px] md:rounded-[20px] "
-                />
-            </div>
-            <div class="w-[50%] px-6  flex items-center justify-center">
-                <div>
-                    <h1 class="text-[30px] font-semibold mb-4">
+            <div class="content">
+                <div class="text-container">
+                    <h1 class="head-1">
                         بيت العباقرة: تعلم، ذاكر، وحقق النجاح بكل سهولة!
                     </h1>
-                    <p class="text-[18px] mb-2 text-gray-400">
+                    <p class="body-1">
                         لقد وصلت إلى بيت العباقرة، حيث تتجلى المعرفة وتتحقق الإبداعات! استمتع بأفضل الكورسات التعليمية عبر الإنترنت مع مذكرات شاملة وحلول متقدمة تساعدك على التفوق وتحقيق أهدافك التعليمية بكل يسر وسهولة.
                     </p>
-                    <h4 class="font-medium mb-3 text-[20px] max-w-[600px]">
+                    <h4 class="head-2">
                         منصة بيت العباقرة للتعليم الالكتروني
                         كل ما يحتاجه طالب الاقتصاد في تطبيق واحد
                     </h4>
-                    <ul class="mb-6">
-                        <li class="mb-4 text-[18px]">✅ خبرة في إنشاء وصناعة المحتوى التعليمي الالكتروني ساهمت في نجاح آلاف الطلاب في حوالي 40 كورس خلال 4 سنوات
+                    <ul class="list">
+                        <li class="list-item">✅ خبرة في إنشاء وصناعة المحتوى التعليمي الالكتروني ساهمت في نجاح آلاف الطلاب في حوالي 40 كورس خلال 4 سنوات
                         </li>
-                        <li class="mb-4 text-[18px]">
+                        <li class="list-item">
                             ✅ تصميم جذاب وسهل الاستخدام يقدم تجربة مستخدم ممتعة
                         </li>
-                        <li class="mb-4 text-[18px]">
+                        <li class="list-item">
                             ✅ دعم تقني وخدمة عملاء سريعة
                         </li>
                     </ul>
-                    <q class="text-red-400 text-[20px] block mb-4">جاهز للانطلاق! الآن على أندرويد وقريبًا على آيفون. كن من أوائل المستفيدين من بيت العباقرة، حيث تبدأ رحلتك نحو التفوق والنجاح.</q>
-                    <div class="flex items-center justify-center">
-                        <a href="{{ route('downloadApk') }}" class="custom-btn max-w-[200px] text-[20px] px-4 py-1 capitalize bg-[#FF8C00] rounded-full flex items-center justify-center">
+                    <q class="note">جاهز للانطلاق! الآن على أندرويد وقريبًا على آيفون. كن من أوائل المستفيدين من بيت العباقرة، حيث تبدأ رحلتك نحو التفوق والنجاح.</q>
+                    <div class="download-btn-container">
+                        <a href="{{ route('downloadApk') }}" class="custom-btn">
                             download app
                         </a>
                     </div>
                 </div>
             </div>
         </div>
-        <footer class="footer px-6 mt-5">
+        <footer class="footer">
             <h1>للاستفسار زيارة مكتبة بيت العباقرة أو التواصل واتساب 0945364375</h1>
         </footer>
     </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
