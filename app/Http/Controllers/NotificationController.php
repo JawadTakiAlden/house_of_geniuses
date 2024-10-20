00c589b4935df5d079967017915acb1c378a5170
JawadTakiAlden <jawad.taki.alden2002@gmail.com>
2024-10-21 00:13:08 +0300

    debug notification

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -40,7 +40,6 @@ class NotificationController extends Controller
     public function sendNotificationForAllUser(SendNotificationRequest $request){
         try {
             $tokens = User::where("phone" , "0948966976")->whereNotNull('device_notification_id')->pluck('device_notification_id')->all();
-            return $tokens;
             $result = $this->BasicSendNotification($request->title , $request->body , $tokens);
             return $result;
         }catch (\Throwable $th){
