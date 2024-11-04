a7d071f0f9ee8e809c99f2a2cc9143fc37620241
JawadTakiAlden <jawad.taki.alden2002@gmail.com>
2024-11-04 14:22:18 +0300

    image api

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,41 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\HelperFunction;
 use Illuminate\Http\Request;
 use Intervention\Image\Laravel\Facades\Image;
 
 class ImageController extends Controller
 {
     public function getImage(Request $request){
+        try {
+            $filepath = $request->query('filepath', 1);
 
-        $filepath = $request->query('filepath', 1);
-        $filePath = public_path($filepath);
+            $filePath = public_path($filepath);
 
-        if (!file_exists($filePath)) {
-            abort(404, 'Image not found.');
-        }
-
-        $scaleX = $request->query('scaleX', 1);
-        $scaleY = $request->query('scaleY', 1);
-        $scale = $request->query('scale', 1);
-        $height = $request->query('height', 0);
-        $width = $request->query('width', 0);
-
-
-        $img = Image::read($filePath);
-
-        if ($width == null && $height == null){
-            if ($scale !== null){
-             $width = $img->width() * $scale ;
-            $height = $img->height() * $scale ;
-            }else {
-                $width = $img->width() * $scaleX ;
-            $height = $img->height() * $scaleY ;
+            if (!file_exists($filePath)) {
+                abort(404, 'Image not found.');
             }
 
-        }
-        $img->resize($width, $height);
+            $scaleX = $request->query('scaleX', 1);
+            $scaleY = $request->query('scaleY', 1);
+            $scale = $request->query('scale', 1);
+            $height = $request->query('height', 0);
+            $width = $request->query('width', 0);
 
-        return $img;
+
+            $img = Image::read($filePath);
+
+            if ($width == null && $height == null){
+                if ($scale !== null){
+                    $width = $img->width() * $scale ;
+                    $height = $img->height() * $scale ;
+                }else {
+                    $width = $img->width() * $scaleX ;
+                    $height = $img->height() * $scaleY ;
+                }
+
+            }
+            $img->resize($width, $height);
+
+            return $img;
+        }catch(\Throwable $th){
+            return HelperFunction::ServerErrorResponse($th);
+        }
+
     }
 }
