06287bad6658aaa87d4a1466f802a750320eb8eb
JawadTakiAlden <jawad.taki.alden2002@gmail.com>
2024-11-04 15:14:27 +0300

    image api

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -39,7 +39,7 @@ class ImageController extends Controller
             }
             $img->resize($width, $height);
 
-               return$img->toPng();
+               return response()->file($img->toPng());
         }catch(\Throwable $th){
             return HelperFunction::ServerErrorResponse($th);
         }
