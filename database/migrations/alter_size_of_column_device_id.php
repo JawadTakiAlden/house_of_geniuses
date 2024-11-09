7de7254ba2befb17de2d0ec595e9809d26f39178
JawadTakiAlden <jawad.taki.alden2002@gmail.com>
2024-11-09 16:59:46 +0300

    fix

--- database/migrations/alter_size_of_column_device_id.php
+++ database/migrations/alter_size_of_column_device_id.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('device_id' , 2000)->nullable()->unique()->change();
+            $table->string('device_id' , 500)->nullable()->unique()->change();
         });
     }
 
