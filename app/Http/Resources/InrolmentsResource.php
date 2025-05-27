b3f591fe4b34b5dfbf3045c66dfd8b7124a1ceca
JawadTakiAlden <jawad.taki.alden2002@gmail.com>
2025-05-27 02:40:21 -0700

    debug

--- app/Http/Resources/InrolmentsResource.php
+++ app/Http/Resources/InrolmentsResource.php
@@ -21,7 +21,9 @@ class InrolmentsResource extends JsonResource
             'image' => $this->course->image ? HelperFunction::getImage($this->course->image) : null,
             'telegram_channel_link' => $this->course->telegram_channel_link,
             'created_at' => $this->created_at->diffForHumans(),
-            "activation_code" => $this->activationCode->type
+            "activation_code" => [
+                "type" => $this->activationCode
+            ]
         ];
     }
 }
