efe8bb6a20da75e262de6628f1491a3da48222f4
Jawad2003Taki <jawad2002taki@gmail.com>
2025-08-30 21:09:30 +0300

    work

--- app/Http/Requests/VideoUploadRequest.php
+++ app/Http/Requests/VideoUploadRequest.php
@@ -21,10 +21,12 @@ class VideoUploadRequest extends FormRequest
      */
     public function rules(): array
     {
+
         ini_set('upload_max_filesize', '100M');
         ini_set('post_max_size', '100M');
         ini_set('max_execution_time', 300);
         ini_set('max_input_time', 300);
+        ini_set('memory_limit', value: "256M");
 
         return [
             "video" => "required|file|max:102400"
