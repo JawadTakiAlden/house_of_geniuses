6ca90693ea52cb5e27809cf8ea4d8234bed90acd
Jawad2003Taki <jawad2002taki@gmail.com>
2025-10-03 23:34:27 +0300

    debug

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -401,7 +401,7 @@ class CourseController extends Controller
             // return $this->success(null, __('messages.course_controller.enroll_successfully', ['course_name' => $course->name]));
         } catch (\Throwable $th) {
             DB::rollBack();
-            return HelperFunction::ServerErrorResponse($th->getMessage());
+            return HelperFunction::ServerErrorResponse($th);
         }
     }
     public function inrollInCourse(SignInCourseRequest $request, $courseID)
